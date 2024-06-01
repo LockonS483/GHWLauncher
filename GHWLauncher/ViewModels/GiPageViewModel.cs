7ae73fcd03db1e3d745ae8c1a0783d9bf49a6f9f
LockonS483 <rumblie.gg@gmail.com>
2024-05-31 20:07:37 -0700

    Gi page bg and layout

--- GHWLauncher/ViewModels/GiPageViewModel.cs
+++ GHWLauncher/ViewModels/GiPageViewModel.cs
@@ -10,6 +10,5 @@ namespace GHWLauncher.ViewModels;
 public partial class GiPageViewModel : ViewModelBase
 {
     //public Bitmap? GiBackImage { get; } = ImageHelper.LoadFromResource(new Uri("avares://GHWLaucher/Assets/abstract.jpg"));
-    //public Task<Bitmap?> GiBackImage { get; } = ImageHelper.LoadFromWeb(new Uri("https://upload.wikimedia.org/wikipedia/commons/4/41/NewtonsPrincipia.jpg"));
-    
+    public Task<Bitmap?> GiBackImage { get; } = ImageHelper.LoadFromWeb(new Uri("https://upload.wikimedia.org/wikipedia/commons/4/41/NewtonsPrincipia.jpg"));
 }
